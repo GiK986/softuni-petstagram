774207b09b7d013684c651c025bdc288adcc3ef2
gik986 <g.kyosev86@gmail.com>
2023-06-10 20:51:26 +0300

    Creating the Photo Model

--- softuni_petstagram/photos/admin.py
+++ softuni_petstagram/photos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from softuni_petstagram.photos.models import Photo
+
 
 # Register your models here.
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('photo', 'description', 'location', 'date_of_publication')
+
+    @staticmethod
+    def get_tagged_pets(obj):
+        return ', '.join([p.name for p in obj.tagged_pets.all()])
