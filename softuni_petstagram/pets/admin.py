eeff621e70125c22bc1f82b7b3556d604b198bdf
gik986 <g.kyosev86@gmail.com>
2023-06-10 20:13:27 +0300

    Added PetAdmin

--- softuni_petstagram/pets/admin.py
+++ softuni_petstagram/pets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from softuni_petstagram.pets.models import Pet
+
 
 # Register your models here.
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'personal_photo', 'date_of_birth', 'slug')
