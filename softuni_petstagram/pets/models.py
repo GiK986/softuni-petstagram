8ea8a3798a791c61ec49ade8c6ec26b3e24d113a
gik986 <g.kyosev86@gmail.com>
2023-06-10 16:52:48 +0300

    Fixed Pet Model date_of_birth

--- softuni_petstagram/pets/models.py
+++ softuni_petstagram/pets/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Pet(models.Model):
     name = models.CharField(max_length=30)
     personal_photo = models.URLField()
-    date_of_birth = models.DateField(Blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
     slug = models.SlugField(unique=True)
