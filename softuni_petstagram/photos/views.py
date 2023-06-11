ed467af7f4c776bbf3aa8d3dae91feda8d4e581b
gik986 <g.kyosev86@gmail.com>
2023-06-11 15:28:02 +0300

    Add models to Photo Details Page

--- softuni_petstagram/photos/views.py
+++ softuni_petstagram/photos/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from softuni_petstagram.photos.models import Photo
+
 
 # Create your views here.
 def add_photo(request):
@@ -7,7 +9,16 @@ def add_photo(request):
 
 
 def photo_details(request, pk):
-    return render(request, 'photos/photo-details-page.html')
+    photo = Photo.objects.get(pk=pk)
+    likes = photo.like_set.all()
+    comments = photo.comment_set.all()
+    context = {
+        'photo': photo,
+        'likes': likes,
+        'comments': comments,
+    }
+
+    return render(request, 'photos/photo-details-page.html', context=context)
 
 
 def edit_photo(request, pk):
