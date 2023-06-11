3fd5268205d3275919259a7a391d37c98a1d85a2
gik986 <g.kyosev86@gmail.com>
2023-06-11 15:03:44 +0300

    Add models to Pet Details Page

--- softuni_petstagram/pets/views.py
+++ softuni_petstagram/pets/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from softuni_petstagram.pets.models import Pet
+
 
 # Create your views here.
 def add_pet(request):
@@ -7,7 +9,14 @@ def add_pet(request):
 
 
 def pet_details(request, username, pet_slug):
-    return render(request, 'pets/pet-details-page.html')
+    pet = Pet.objects.get(slug=pet_slug)
+    all_photos = pet.photo_set.all()
+    context = {
+        'pet': pet,
+        'all_photos': all_photos,
+    }
+
+    return render(request, 'pets/pet-details-page.html', context=context)
 
 
 def edit_pet(request, username, pet_slug):
