48784305057a5c80752e51ee14d69d834e01c588
gik986 <g.kyosev86@gmail.com>
2023-06-30 01:23:56 +0300

    Creating a Comment Form

--- softuni_petstagram/common/views.py
+++ softuni_petstagram/common/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, resolve_url
 from pyperclip import copy
 
-
+from softuni_petstagram.common.forms import CommentForm
 from softuni_petstagram.common.models import Like
 from softuni_petstagram.photos.models import Photo
 
@@ -9,9 +9,11 @@ from softuni_petstagram.photos.models import Photo
 # Create your views here.
 def index(request):
     all_photos = Photo.objects.all()
+    comment_form = CommentForm()
 
     context = {
         'all_photos': all_photos,
+        'comment_form': comment_form,
     }
 
     return render(request, 'common/home-page.html', context=context)
@@ -32,5 +34,17 @@ def like_functionality(request, photo_id):
 
 def share_functionality(request, photo_id):
     copy(request.META.get('HTTP_HOST') + resolve_url('photo details', pk=photo_id))
+
+    return redirect(request.META.get('HTTP_REFERER') + f'#{photo_id}')
+
+
+def comment_functionality(request, photo_id):
+    if request.method == 'POST':
+        photo = Photo.objects.get(pk=photo_id)
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.to_photo = photo
+            comment.save()
 
     return redirect(request.META.get('HTTP_REFERER') + f'#{photo_id}')
